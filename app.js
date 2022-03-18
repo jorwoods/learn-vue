7732b9c348a338896b1582f874f1a34387ee4a4c
Jordan Woods <lenluin@gmail.com>
2022-03-17 21:07:21 -0500

    Add a for loop into the page

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = Vue.createApp({
         // Which returns an object. The keys from that object are accessible in the DOM.
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
             x: 0,
             y: 0,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss',  img: "assets/1.jpg", isFav: false},
+                {title: 'the way of kings', author: 'brandon sanderson', img: "assets/2.jpg", isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', img: "assets/3.jpg", isFav: false},
+            ]
         }
     },
     methods: {
@@ -32,6 +34,10 @@ const app = Vue.createApp({
         handleMouseMove (e) {
             this.x = e.offsetX
             this.y = e.offsetY
+        },
+
+        toggleFav (book) {
+            book.isFav = !book.isFav
         }
     }
 })
