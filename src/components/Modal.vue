eff60271eda34c4f5e03260863586d92a981ae79
Jordan Woods <lenluin@gmail.com>
2022-03-18 22:57:48 -0500

    Style links

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -41,11 +41,30 @@ export default {
     border: none;
     padding: 0;
   }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
   .modal.sale{
     background: crimson;
     color: white;
   }
   .modal.sale h1 {
+    color: white;
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
     color: white;
   }
 </style>
