8a71e9696c6ed354f49942c84b834a929f24f8aa
Jordan Woods <lenluin@gmail.com>
2022-03-18 22:58:03 -0500

    Formatting

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,47 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-  <!-- Below only works after it has been imported and registered as a component -->
-  <Modal theme="sale" @close="toggleModal">
-    <template v-slot:links>
-      <a href="#">Sign up now!</a>
-      <a href="#">more info</a>
-    </template>
-    <h1>Giveaway!</h1>
-    <p>Grab your swag for half price!</p>
-  </Modal>
+    <!-- Below only works after it has been imported and registered as a component -->
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now!</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Giveaway!</h1>
+      <p>Grab your swag for half price!</p>
+    </Modal>
   </div>
   <button @click.alt="toggleModal">open modal (alt)</button>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
-      title: 'My First Vue App :)',
+      title: "My First Vue App :)",
       header: "Sign up for the Giveaway!",
       text: "Grab your swag for half price!",
       showModal: false,
-    }
+    };
   },
   components: {
-    Modal
+    Modal,
   },
   methods: {
-    handleClick () {
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
     },
 
-    toggleModal () {
-      this.showModal = ! this.showModal
-    }
-  }
-}
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
