bca63aff82a7f17eef06cababc8634dca62e40a8
Jordan Woods <lenluin@gmail.com>
2022-03-20 16:19:35 -0500

    Enable two way data binding

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,14 +1,50 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="text" required>
+    <input type="email" required v-model="email" />
+
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>
+        Accept terms and conditions
+      </label>
+    </div>
+
+    <!-- This block is how you pass multiple values from checkboxes into a single vue data element -->
+    <!-- <div><input type="checkbox" v-model="names" value="Luigi"><label>Luigi</label></div>
+    <div><input type="checkbox" v-model="names" value="Yoshi"><label>Yoshi</label></div>
+    <div><input type="checkbox" v-model="names" value="Mario"><label>Mario</label></div> -->
+
   </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms: {{ terms }}</p>
+  <!-- <p>Names: {{ names }}</p> -->
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      email: "mario",
+      password: "",
+      role: "",
+      terms: false,
+      // names: [],
+    };
+  },
+};
 </script>
 
 <style>
@@ -29,7 +65,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -37,5 +74,12 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
