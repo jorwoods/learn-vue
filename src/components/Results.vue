6455b5c6878ceb157b57f4481d872d376ecc433f
Jordan Woods <lenluin@gmail.com>
2022-03-20 14:25:49 -0500

    Style rank

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,6 @@
 <template>
-  <p>Reaction time: {{ score }}ms {{ rank }}</p>
+  <p>Reaction time: {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
@@ -23,5 +24,9 @@ export default {
 </script>
 
 <style>
-
+ .rank {
+   font-size: 1.4em;
+   color: #0faf87;
+   font-weight: bold;
+ }
 </style>
