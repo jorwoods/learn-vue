87dec4cf80b13c753674a76a0c3cc50af914352e
Jordan Woods <lenluin@gmail.com>
2022-03-20 21:51:02 -0500

    Add redirects

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,30 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/Jobs.vue")
+    component: () => import(/* webpackChunkName: "about" */ "../views/jobs/Jobs.vue")
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/jobs/JobDetails.vue"),
+    // Sends a route parameter to props of the target component
+    props: true,
+  },
+  //redirect
+  {
+    path: "/all-jobs",
+    redirect: "/jobs",
+  },
+  // catchall 404
+  {
+    // catchAll is a function that accepts a regex.
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: () => import(/* webpackChunkName: "about" */ "../views/NotFound.vue"),
+
   },
 ]
 
---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Job Details Page</h1>
+  <!-- "id" below matches whatever the route parameter is called in index.js. Since it is :id in index.js, it is id here. -->
+  <p>The job id is {{ id }}</p>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      // id: this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
