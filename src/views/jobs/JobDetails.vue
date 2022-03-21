1e974aecf292d0def0ba3a55d94cbf739b9872a7
Jordan Woods <lenluin@gmail.com>
2022-03-21 08:46:41 -0500

    Conditionally output content

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,8 +1,13 @@
 <template>
-  <h1>{{ job.title }}</h1>
-  <!-- "id" below matches whatever the route parameter is called in index.js. Since it is :id in index.js, it is id here. -->
-  <p>The job id is {{ id }}</p>
-  <p>{{ job.detail }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <!-- "id" below matches whatever the route parameter is called in index.js. Since it is :id in index.js, it is id here. -->
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
 </template>
 
 <script>
