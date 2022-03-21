d4ed6b0fa8829c75b7b9862b765974286c5f1444
Jordan Woods <lenluin@gmail.com>
2022-03-20 21:32:02 -0500

    Basics of routing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-
+ <div id="nav">
+   <router-link to="/">Home</router-link> |
+   <router-link :to="{name: 'About'}">About</router-link> |
+   <router-link :to="{name: 'Jobs'}">Jobs</router-link>
+ </div>
+ <router-view/>
 </template>
 
 <script>
@@ -16,5 +21,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+#nav{
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+#nav a.router-link-exact-active {
+  color: #24b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="home">
+  <h1>Homepage</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo maiores nisi recusandae ex minima at, quo inventore id quam cumque ullam odit unde optio libero aliquam. Eum delectus sunt architecto molestias qui, dolorem doloribus reiciendis, veritatis aspernatur illo exercitationem corporis accusantium laudantium nisi. Ea in deleniti sit. Sunt similique sint odit suscipit ducimus nesciunt! Aperiam sunt molestias voluptates modi molestiae magnam accusamus nostrum possimus perferendis rem mollitia quaerat, dolorem odio exercitationem tenetur nemo ullam voluptate voluptatibus. Fuga necessitatibus in architecto? Eum magni vel fuga vero, quia est praesentium modi pariatur veritatis labore, obcaecati debitis dicta blanditiis nulla accusantium temporibus aut?</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo maiores nisi recusandae ex minima at, quo inventore id quam cumque ullam odit unde optio libero aliquam. Eum delectus sunt architecto molestias qui, dolorem doloribus reiciendis, veritatis aspernatur illo exercitationem corporis accusantium laudantium nisi. Ea in deleniti sit. Sunt similique sint odit suscipit ducimus nesciunt! Aperiam sunt molestias voluptates modi molestiae magnam accusamus nostrum possimus perferendis rem mollitia quaerat, dolorem odio exercitationem tenetur nemo ullam voluptate voluptatibus. Fuga necessitatibus in architecto? Eum magni vel fuga vero, quia est praesentium modi pariatur veritatis labore, obcaecati debitis dicta blanditiis nulla accusantium temporibus aut?</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
