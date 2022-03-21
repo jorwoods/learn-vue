87b779dd614f848132b8373021eb49707b3292ff
Jordan Woods <lenluin@gmail.com>
2022-03-21 08:42:48 -0500

    Fetch data from jobs api

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,7 +1,8 @@
 <template>
-  <h1>Job Details Page</h1>
+  <h1>{{ job.title }}</h1>
   <!-- "id" below matches whatever the route parameter is called in index.js. Since it is :id in index.js, it is id here. -->
   <p>The job id is {{ id }}</p>
+  <p>{{ job.detail }}</p>
 </template>
 
 <script>
@@ -9,8 +10,14 @@ export default {
   props: ['id'],
   data() {
     return {
-      // id: this.$route.params.id
+      job: null
     }
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
