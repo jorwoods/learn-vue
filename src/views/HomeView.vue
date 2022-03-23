cb22ca3e51ddbfd15ca6619d3e0c3c639f45b73a
Jordan Woods <lenluin@gmail.com>
2022-03-23 12:21:33 -0500

    Use refs in composition API to make things reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      Home
+    </h1>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">Add one to age.</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+  },
+  setup() {
+    console.log("setup")
+    const name = ref('mario')
+    const age = ref(30)
+
+    const handleClick = () => {
+      name.value = 'luigi'
+    }
+
+    return {
+      name,
+      age,
+      handleClick,
+    }
+  },
 }
 </script>
