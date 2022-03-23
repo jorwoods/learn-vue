76957cc53b4207c8bf44c53c2874dd58dfd4b420
Jordan Woods <lenluin@gmail.com>
2022-03-23 12:29:14 -0500

    Explore using reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,18 @@
     <h1>
       Home
     </h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">Add one to age.</button>
-    <input type="text" v-model="name">
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja One</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 
 export default {
@@ -21,18 +23,19 @@ export default {
   },
   setup() {
     console.log("setup")
-    const name = ref('mario')
-    const age = ref(30)
+    const ninjaOne = ref({name: "mario", age: 30})
+    // Primitive values do not work with reactive.
+    const ninjaTwo = reactive({name: "luigi", age: 35})
 
-    const handleClick = () => {
-      name.value = 'luigi'
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40
     }
 
-    return {
-      name,
-      age,
-      handleClick,
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40
     }
+
+    return {ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo}
   },
 }
 </script>
