95317b7c12a04840ea207fed278e726b5931a55c
Jordan Woods <lenluin@gmail.com>
2022-03-26 15:34:48 -0500

    Factor load function to separate file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,51 +3,26 @@
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
     <div v-if="posts.length">
-      <PostList v-if="showPosts" :posts="posts" />
+      <PostList :posts="posts" />
     </div>
     <div v-else>Loading...</div>
-    <button @click="showPosts = !showPosts">toggle posts</button>
-    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import PostList from '../components/PostList.vue'
-import { ref } from 'vue'
-
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts")
-        console.log(data)
-
-        if (!data.ok){
-          throw Error("no data available")
-        }
-
-        posts.value = await data.json()
-
-
-      } catch (err) {
-        // update the error ref's value with the content of the error message
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
+    const { posts, error, load } = getPosts()
 
     load()
 
-    const showPosts = ref(true)
-
-    return { posts, showPosts, error }
+    return { posts, error }
   }
 }
 </script>
