465a2e782ca2472160a919033f9bab67644bb48a
Jordan Woods <lenluin@gmail.com>
2022-03-26 09:58:28 -0500

    Pass props using composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,42 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name"> {{ name }}</div>
-    <button @click="handleClick">Stop Watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 
 export default {
   name: 'HomeView',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+    const posts = ref([
+      {title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolore ipsum laborum quas? Amet necessitatibus perferendis repellendus ab quis, sequi officiis quibusdam beatae accusantium eius nisi asperiores modi quod vitae.' , id: 1},
+      {title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2},
+    ])
 
-    const stopWatch = watch(search, () => {
-      console.log("watch function ran")
-    })
-
-    const stopWatchEffect = watchEffect(() => {
-      console.log("watchEffect function ran", search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter(name => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopWatchEffect()
-    }
-
-    return { names, search, matchingNames, handleClick }
+    return { posts }
   }
 }
 </script>
