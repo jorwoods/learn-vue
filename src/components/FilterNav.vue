e645db8cf0042c60997e1c97da9945b40a23ca25
Jordan Woods <lenluin@gmail.com>
2022-04-16 14:54:24 -0500

    Add filter function for tasks

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="filter-nav">
+    <button :class="{ active: currentFilter === 'all'}" @click="updateFilter('all')">View All</button>
+    <button :class="{ active: currentFilter === 'completed'}" @click="updateFilter('completed')">Completed</button>
+    <button :class="{ active: currentFilter === 'ongoing'}" @click="updateFilter('ongoing')">View ongoing</button>
+  </nav>
+</template>
+
+
+<script setup lang="ts">
+import { defineEmits, defineProps, ref } from 'vue'
+
+const emits = defineEmits(["filterChange"])
+const props = defineProps(['currentFilter'])
+
+const updateFilter = (filterBy: string) => {
+  emits('filterChange', filterBy)
+}
+
+
+</script>
+
+
+<style>
+ .filter-nav button {
+   background: none;
+   border: none;
+   color: #bbb;
+   outline: none;
+   font-size: 12px;
+   text-transform: uppercase;
+   margin-right: 10px;
+   letter-spacing: 1px;
+   font-weight: bold;
+   cursor: pointer;
+ }
+
+ .filter-nav button.active {
+   color: #555;
+ }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="currentFilter = $event" :currentFilter="currentFilter" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
@@ -9,14 +10,17 @@
 </template>
 
 <script setup lang="ts">
-import { defineComponent, inject, onMounted, ref } from "vue";
+import { defineComponent, inject, computed, onMounted, ref } from "vue";
 import loadProjects from "@/func/getProjects";
 import ProjectItem from "@/types/ProjectItem";
 import SingleProject from "@/components/SingleProject.vue"
+import FilterNav from "@/components/FilterNav.vue"
 
 const database_url = inject("database_url") as string
 
 let { projects, error, load } = loadProjects(database_url);
+
+let currentFilter = ref("all")
 
 load()
 
@@ -32,5 +36,15 @@ const handleComplete = (id: number) => {
   let p = projects.value.find(project => project.id === id)
   p!.complete = !p?.complete
 }
+
+const filteredProjects = computed(() => {
+  if (currentFilter.value === 'completed'){
+    return projects.value.filter((project) => project.complete)
+  }
+  else if (currentFilter.value === 'ongoing') {
+    return projects.value.filter((project) => !project.complete)
+  }
+  return projects.value.filter(p => true)
+})
 
 </script>
