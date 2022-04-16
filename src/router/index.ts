09e3265472f0cac6e78536a4b5809d18c62250bf
Jordan Woods <lenluin@gmail.com>
2022-04-16 10:57:50 -0500

    Add form to add project

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,14 @@ const routes: Array<RouteRecordRaw> = [
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   // }
+  {
+    path: '/add',
+    name: 'AddProject',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '@/views/AddProject.vue')
+  }
 ]
 
 const router = createRouter({
