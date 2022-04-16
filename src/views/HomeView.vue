a8897c80a9c6760c7eedda4213a2b3d362ccec7b
Jordan Woods <lenluin@gmail.com>
2022-04-16 10:21:22 -0500

    Refactor HomeView to use script setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,37 +8,29 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent, inject, onMounted, ref } from "vue";
 import loadProjects from "@/func/getProjects";
 import ProjectItem from "@/types/ProjectItem";
 import SingleProject from "@/components/SingleProject.vue"
 
-export default defineComponent({
-  name: "HomeView",
-  components: { SingleProject },
-  setup() {
+const database_url = inject("database_url") as string
 
-    const database_url = inject("database_url") as string
+let { projects, error, load } = loadProjects(database_url);
 
-    let { projects, error, load } = loadProjects(database_url);
+load()
 
-    load()
+projects = ref(projects)
 
-    projects = ref(projects)
+const handleDelete = (id: number) => {
+  projects.value = projects.value.filter((project) => {
+    return project.id != id
+  })
+}
 
-    const handleDelete = (id: number) => {
-      projects.value = projects.value.filter((project) => {
-        return project.id != id
-      })
-    }
+const handleComplete = (id: number) => {
+  let p = projects.value.find(project => project.id === id)
+  p!.complete = !p?.complete
+}
 
-    const handleComplete = (id: number) => {
-      let p = projects.value.find(project => project.id === id)
-      p!.complete = !p?.complete
-    }
-
-    return { handleComplete, handleDelete, projects }
-  },
-});
 </script>
